040ab68a130de65b6f2bcf4efd181e5d9c1931e3
花海 <137281299@qq.com>
2025-10-15 01:11:02 +0800

    ai生成基础架构后修改用户服务依赖

---
+++ backend/user-parent/user-boot/src/main/java/top/flowerstardream/hcd/App.java
@@ -0,0 +1,27 @@
+package top.flowerstardream.hcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 花海
+ * @Date: 2025/10/14/21:36
+ * @Description: 启动入口
+ */
+@SpringBootApplication
+@EnableTransactionManagement //开启注解方式的事务管理
+@EnableScheduling //开启注解方式的定时任务
+@EnableCaching //开启注解方式的缓存管理
+@Slf4j
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+        log.info("server started");
+    }
+}
